782982480642942711ebedb8ed3612b2e3950442
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-08-12 18:21:50 +0530

    fixed design changes and implemented features in vendor

--- vendors/utils.py
+++ vendors/utils.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import csv
 from io import BytesIO
 from xhtml2pdf import pisa
-
+from assets.models import Asset
 
 def render_to_csv(context_dict={}):
     response = HttpResponse(content_type='text/csv')
@@ -22,3 +22,11 @@ def render_to_pdf(template_src, context_dict={}):
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
+
+def get_count_of_assets(request,vendor_id):
+    """
+    Returns the count of assets for the given organization.
+    """
+    get_count=Asset.objects.filter(organization=request.user.organization,vendor=vendor_id, is_deleted=False).count()
+    print("get_count", get_count)
+    return get_count
