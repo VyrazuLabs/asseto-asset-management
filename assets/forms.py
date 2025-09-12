edf725184ed6a72ca90e01b25d80888ccb2e8d8e
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-09-12 16:39:09 +0530

    changes in frontend of assets and fixed csv upload

--- assets/forms.py
+++ assets/forms.py
@@ -25,7 +25,7 @@ class AssetForm(forms.ModelForm):
             attrs={'class': 'form-select'}
         )
     )
-    tag =  forms.CharField(required=False, widget=forms.TextInput(
+    tag =  forms.CharField(required=True, widget=forms.TextInput(
         attrs={'autocomplete': 'off', 'class': 'form-control',
                'placeholder': 'Enter Asset Tag'}
     ))
