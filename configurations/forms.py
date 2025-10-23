db6249fc404da87106b913e63b55f3e153cf147f
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-10-23 18:33:30 +0530

    history.html

--- configurations/forms.py
+++ configurations/forms.py
@@ -11,3 +11,19 @@ class TagConfigurationForm(forms.ModelForm):
 
         # Now call parent's __init__ with remaining args/kwargs
         super().__init__(*args, **kwargs)
+
+    def clean_prefix(self):
+        prefix = self.cleaned_data.get('prefix')
+
+        if not prefix.isalpha():  # checks only letters
+            raise forms.ValidationError("Prefix must contain only alphabets (A-Z).")
+        return prefix
+
+    def clean_number_suffix(self):
+        suffix = self.cleaned_data.get('number_suffix')
+
+        if not suffix.isdigit():  # checks only numbers
+            raise forms.ValidationError("Suffix must contain only numbers (0-9).")
+        return suffix
+
+    
