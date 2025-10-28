ec198672fa250fc5c1fba813fac8dfbc8d97c199
Rajdeep <rajdeep.291@vyrazu.com>
2025-10-28 15:40:39 +0530

    add db choices

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -1,26 +1,32 @@
-import os
-from django.db import connection, connections
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.db import connections, DEFAULT_DB_ALIAS
+from django.db.utils import OperationalError, ConnectionDoesNotExist
+from django.conf import settings
 
-from AssetManagement.settings import BASE_DIR
-    
 class DBConnectionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        print('middlewere running!')
         skip_paths = [
             reverse('authentication:introduce'),
             reverse('authentication:db_configure'),
+            reverse('authentication:register'),
         ]
+
+
         if request.path in skip_paths:
             return self.get_response(request)
-        
 
-        if not os.environ.get('DB_NAME'):
+        try:
+            conn = connections[DEFAULT_DB_ALIAS]
+            conn.ensure_connection()
+        except OperationalError as e:
             return redirect('authentication:introduce')
-        
+        except ConnectionDoesNotExist as e:
+            return redirect('authentication:introduce')
+        except Exception as e:
+            return redirect('authentication:introduce')
+
         return self.get_response(request)
-            
