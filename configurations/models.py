cd2d07bc5895bb11d298d6a8864dce7a75866004
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-11-06 12:00:16 +0530

    slack implementation final

--- configurations/models.py
+++ configurations/models.py
@@ -1,6 +1,11 @@
-from django.db import models
 import uuid
+import base64
+import binascii
+
+from django.db import models
 from dashboard.models import Organization
+from authentication.models import User
+
 class BrandingImages(models.Model):
 
     logo_path="/logo/"
@@ -44,6 +49,7 @@ class Extensions(models.Model):
         (1, 'Active'),
     ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description=models.TextField(blank=True, null=True)
     organization = models.ForeignKey(
         Organization,
         on_delete=models.CASCADE,
@@ -55,6 +61,39 @@ class Extensions(models.Model):
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     payment_date = models.DateTimeField(auto_now_add=True)
     validity= models.IntegerField( default=0)
-    
 
-    
+class SlackConfiguration(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="slack_configuration")
+    slack_user_id = models.CharField(max_length=100, null=True, blank=True)
+    access_token = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    team_id=models.CharField(max_length=100, null=True, blank=True)
+    channel_id=models.CharField(max_length=100, null=True, blank=True)
+    client_id=models.CharField(max_length=100, null=True, blank=True)
+    client_secret=models.CharField(max_length=100, null=True, blank=True)
+
+    # @staticmethod
+    # def is_valid_base64(string):
+    #     try:
+    #         return string == base64.b64encode(base64.b64decode(string.encode())).decode()
+    #     except Exception:
+    #         return False
+
+    # def save(self, *args, client_id=None, client_secret=None, **kwargs):
+    #     import pdb; pdb.set_trace();
+    #     try:
+    #         client_id = client_id or self.client_id
+    #         if client_id is not None and not self.is_valid_base64(client_id):
+    #             self.client_id = base64.b64encode(client_id.encode()).decode()
+    #     except (TypeError, ValueError, AttributeError, binascii.Error):
+    #         pass
+
+    #     try:
+    #         client_secret = client_secret or self.client_secret
+    #         if client_secret is not None and not self.is_valid_base64(client_secret): 
+    #             self.client_secret = base64.b64encode(client_secret.encode()).decode()
+    #     except (TypeError, ValueError, AttributeError, binascii.Error):
+    #         pass
+
+    #     super().save(*args, **kwargs)
