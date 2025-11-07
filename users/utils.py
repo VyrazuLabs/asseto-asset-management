1d6cc1e04c4863bb70ec392d10300ba8479b6b81
Rajdeep <rajdeep.291@vyrazu.com>
2025-11-07 12:45:40 +0530

    user api

--- users/utils.py
+++ users/utils.py
@@ -86,7 +86,52 @@ def make_fields_optional(form, fields=None):
         if field_name in form.fields:
             form.fields[field_name].required = False
 
+
+
+
 def get_asset_by_users(id):
     get_user=User.objects.filter(id=id).first()
     get_asset=AssignAsset.objects.filter(user=get_user)
     return get_asset
+
+
+def assigned_asset_to_user(page_object):
+    user_ids = [u.id for u in page_object]
+
+    assigned_assets = AssignAsset.objects.filter(user_id__in=user_ids).select_related("asset")
+
+    user_asset_map = {}
+    for aa in assigned_assets:
+        user_asset_map.setdefault(aa.user_id, []).append(aa.asset)
+    
+    return user_asset_map
+
+
+def user_data(user_list):
+    user_data_list=[]
+    for user in user_list:
+        user_data_list.append({
+            'id':user.id,
+            'full_name':user.full_name,
+            'email':user.email,
+            'role':user.role.related_name if user.role else None
+        })
+    return user_data_list
+
+def user_details(get_user,assigned_assets,asset_images):
+    asset_images_list=[]
+    for asset_image in asset_images:
+        asset_images_list.append(asset_image.image.url)
+    print(asset_images_list,type(asset_images_list))
+
+    user_details={
+        'name':get_user.full_name,
+        'email':get_user.email,
+        'phone_number':get_user.phone,
+        'assigned_asset_name':assigned_assets.asset.name,
+        'asset_tag':assigned_assets.asset.tag,
+        'vendor':assigned_assets.asset.vendor.name,
+        'assigned_date':assigned_assets.assigned_date,
+        'asset_image_list':asset_images_list
+    }
+    return user_details
