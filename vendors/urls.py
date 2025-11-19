64eeb0846e94357115eab88eb19961f806f4e4d9
Rajdeep <rajdeep.291@vyrazu.com>
2025-11-19 11:16:06 +0530

    vendor api

--- vendors/urls.py
+++ vendors/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views
-
+from . import views,api_views
 app_name = 'vendors'
 
 urlpatterns = [
@@ -13,4 +12,14 @@ urlpatterns = [
     path('status/<uuid:id>', views.status, name='status'),
     path('csv', views.export_vendors_csv, name='export_vendors_csv'),
     path('pdf', views.export_vendors_pdf, name='export_vendors_pdf'),
+]
+
+vendor_api_urlpatterns = [
+    path('api/vendor/list',api_views.VendorList.as_view(),name='vendor_list'),
+    path('api/vendor/add',api_views.AddVendor.as_view(),name='add_vendor'),
+    path('api/vendor/details/<uuid:id>',api_views.VendorDetails.as_view(),name='vendor_details'),
+    path('api/vendor/update/<uuid:id>',api_views.UpdateVendor.as_view(),name='update_vendor'),
+    path('api/vendor/delete/<uuid:id>',api_views.DeleteVendor.as_view(),name='vendor_details'),
+    path('api/vendor/search/',api_views.SearchVendor.as_view(),name='search_vendor'),
+
 ]
