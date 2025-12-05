c02317a63614269f647870c2636fdb8e55a62716
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-12-05 18:39:44 +0530

    fixed minor errors in license

--- assets/barcode.py
+++ assets/barcode.py
@@ -7,6 +7,10 @@ from barcode import Code128
 from barcode.writer import SVGWriter
 from django.contrib.auth.decorators import login_required
 from rest_framework.views import APIView
+from barcode.writer import ImageWriter
+import io
+import base64
+from assets.api_utils import convert_svg_mm_to_px
 
 class Scan_barcode(View):
     def get(self,request,**kwargs):
@@ -30,4 +34,18 @@ def generate_barcode(tag_id):
     barcode_svg = Code128(tag_id, writer=SVGWriter())
     svg_content = barcode_svg.render(writer_options={'write_text': False,'module_width': 0.8,'module_height': 25,},text=f'Asset-{tag_id}').decode('utf-8')
     return svg_content
-    
+
+def generate_barcode_px(tag_id):
+    writer = SVGWriter()
+    writer_options = {
+        "write_text": False,
+        "module_height": 25,   # still internal mm
+        "module_width": 0.8,   # still internal mm
+    }
+
+    raw_svg = Code128(str(tag_id), writer=writer).render(writer_options).decode()
+
+    # convert everything to px
+    svg_px = convert_svg_mm_to_px(raw_svg)
+
+    return svg_px
