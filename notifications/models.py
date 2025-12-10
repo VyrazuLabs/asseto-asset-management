8d37c48dd1a786adb8bf81f7312b6396512b0c42
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2025-12-10 18:51:23 +0530

    worked on notification and other fixes from testers side

--- notifications/models.py
+++ notifications/models.py
@@ -5,9 +5,14 @@ from dashboard.models import TimeStampModel
 import uuid
 from authentication.models import User
 
-# Create your models here.
 
 class Notification(TimeStampModel):
+    NotificationTypeChoices = [
+        (0, 'browser'),
+        (1, 'email'),
+        (2, 'slack'),
+        (3, 'slack')
+    ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     instance_id = models.UUIDField(default=uuid.uuid4, editable=False)
     notification_title = models.CharField(max_length = 255, blank = True, null = True)
@@ -15,7 +20,9 @@ class Notification(TimeStampModel):
     icon = models.CharField(max_length = 225, blank = True, null = True)
     link = models.CharField(max_length = 225, blank = True, null = True)
     is_superuser = models.BooleanField(default=False)
-    
+    entity_type = models.IntegerField(choices=NotificationTypeChoices, default=0)
+
+
 class UserNotification(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank = True, null = True)
     notification = models.ForeignKey(Notification, models.DO_NOTHING, blank = True, null = True)
