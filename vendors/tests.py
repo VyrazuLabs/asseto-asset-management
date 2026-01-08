c904f888026aaca51a12a8e0e579a1bee7ea9521
Rajdeep <rajdeep.291@vyrazu.com>
2026-01-08 19:29:32 +0530

    fix the asset status update

--- vendors/tests.py
+++ vendors/tests.py
@@ -63,10 +63,6 @@ class Asseto_test(TestCase):
         self.assertEqual(self.vendor.address.address_line_one,"Address line 1")
         self.assertEqual(self.vendor.organization.name, "Organization 1")
 
-# class EditVendorTest(TestCase):
-    # def setUp(self):
-    #     self.vendor = Vendor.objects.create(name='Vendor 1', email='vendor1@example.com', phone='1234567890')
-
     def test_edit_vendor(self):
         # Edit the vendor
         self.vendor.name = 'New Name'
@@ -103,61 +99,10 @@ class Asseto_test(TestCase):
         self.assertEqual(self.vendor.organization.date_format,"dd-mm-yyyy")
         self.assertEqual(self.vendor.organization.logo,"logo2.png")
 
-    # def test_edit_vendor_with_same_info(self):
-    #     # Edit the vendor with same information
-    #     self.vendor.name = 'Vendor 1'
-    #     self.vendor.email = 'vendor1@example.com'
-    #     self.vendor.phone = '1234567890'
-
-    #     self.vendor.save()
-
-    #     # Check that the changes were saved correctly
-    #     self.assertEqual(self.vendor.name, 'Vendor 1')
-    #     self.assertEqual(self.vendor.email, 'vendor1@example.com')
-    #     self.assertEqual(self.vendor.phone, '1234567890')
-
-    # def test_edit_vendor_with_required_fields(self):
-    #     # Edit the vendor with required fields (name)
-    #     self.vendor.name = 'New Name'
-
-    #     self.vendor.save()
-
-    #     # Check that the changes were saved correctly
-    #     self.assertEqual(self.vendor.name, 'New Name')
-
-# class TestVendorDeletion(TestCase):
-    # def setUp(self):
-    #     # Create a test vendor object
-    #     user_address=Address.objects.create(
-    #         address_line_one = "Address line 1",
-    #         address_line_two = "Address line 2",
-    #         country = "India",
-    #         state="West Bengal",
-    #         city = "Kolkata",
-    #         pin_code = "700018",
-    #     )
-    #     user_organization = Organization.objects.create(
-    #         name="Organization 1",
-    #         website="asseto.com",
-    #         phone="1234567890",
-    #         email="asseto@asseto.com",
-    #         currency="INR",
-    #         date_format="dd-mm-yyyy",
-    #         logo = "logo.png"
-    #     )
-    #     self.vendor = Vendor(name='Test Vendor', email='test@example.com', phone='7896541230',contact_person='person',
-    #                         designation='developer',gstin_number='21321',description='description 1',
-    #                         address=user_address,organization=user_organization)
-    #     self.vendor.save()
     def test_delete_vendor(self):
-        # Try to delete the vendor object
         self.vendor.delete()
-        
-        # Check if the vendor object is deleted from the database
         with self.assertRaises(Vendor.DoesNotExist):
             Vendor.objects.get(id=self.vendor.id)
-
-        # Check if the address and organization objects are not affected
         address = Address.objects.get(id=self.vendor.address.id)
         organization = Organization.objects.get(id=self.vendor.organization.id)
         self.assertIsNotNone(address)
