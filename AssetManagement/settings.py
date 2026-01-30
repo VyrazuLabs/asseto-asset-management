5d1f000bcedb087486feb4d1cfe8f56a3f5cd13a
Soumyajit Ghosh <soumyajit.ghosh@vyrazu.com>
2026-01-30 16:01:08 +0530

    changed frontend  and fixed other issues

--- AssetManagement/settings.py
+++ AssetManagement/settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    # "authentication.middleware.DynamicCsrfMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
