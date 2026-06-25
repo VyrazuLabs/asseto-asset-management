c7102d34353f51d7a54b60c651a9c67c057c3dd6
Kashis <kashis.308@vyrazu.com>
2026-06-25 14:53:38 +0530

    Fixes done for the error came in client and support ticket list

--- configurations/context_processors.py
+++ configurations/context_processors.py
@@ -1,10 +1,5 @@
 from .models import LocalizationConfiguration, BrandingImages
 from .translations.utils import get_translations
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e67c487 (Removed the code from init.py files for the translation into the utils and constant files)
 
 def translations(request):
     """
